713164add62fbc7b4e72feeb877ca8fee4e678c2
Eoin Bailey <baileye@gmail.com>
2011-03-04 14:30:58 +0000

    Initial Commit

---
+++ src/com/gtug/android/livedrive/liveDrive.java
@@ -0,0 +1,13 @@
+package com.gtug.android.livedrive;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class liveDrive extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
